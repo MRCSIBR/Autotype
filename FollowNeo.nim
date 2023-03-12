71372988a05210567712cc56a670e9e30be73a4f
MRCSIBR <127572358+MRCSIBR@users.noreply.github.com>
2023-03-12 01:22:12 +0000

    Add files via upload

---
+++ FollowNeo.nim
@@ -0,0 +1,20 @@
+import std/random
+import std/os
+
+
+let frase = "The Matrix has you, Neo... , Follow the white rabbit. Knock Knock ! \n\n"
+let frase2= "You ever have the feeling that you're not sure if you're awake or still dreaming? \n"
+
+
+proc tipear(frase: string ) =
+  for letter in frase:
+      # Generate a random number between 92 and 232
+      var dur = rand(108..242)
+      sleep(dur)
+
+      stderr.write letter
+
+tipear(frase)
+tipear(frase2)
+
+# La frase de texto se deberia imprimir en la misma linea [~]
